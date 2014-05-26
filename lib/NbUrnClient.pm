b3478d17641f866d4264ff6f671186550a659d6f
Rune Lain Knudsen <rune.knudsen@uib.no>
2014-05-26 16:10:26 +0200

    Bugfixes and improvement in library and command line scripts.

--- lib/NbUrnClient.pm
+++ lib/NbUrnClient.pm
@@ -4,16 +4,18 @@ package NbUrnClient;
 
 use strict;
 use SOAP::Lite;
+use YAML::Tiny;
+use Data::Dumper;
 
 sub new {
     my $class = shift;
-    my $self = {
-	'url' => "http://www.nb.no/idtjeneste/ws?wsdl",
-	'client' => undef,
-	'sso_token' => undef
-    };
+    my %args = @_;
+    my $self = {};
+    $self->{config_file} = $args{config_file};
+    $self->{sso_token} = undef;
+    $self->{config} = YAML::Tiny->read($args{config_file})->[0]->{config}{urn};
 
-    $self->{client} = SOAP::Lite->new(proxy => 'http://www.nb.no/idtjeneste/ws');
+    $self->{client} = SOAP::Lite->new(proxy => $self->{config}->{proxy});
     $self->{client}->ns('http://nb.no/idservice/v1.0/', 'ns1');
     $self->{client}->autotype(0);
 
@@ -80,13 +82,13 @@ sub get_next_urn {
     my ($self, $seriesCode) = @_;
 
 #     if (defined $self->{sso_tdie "Not implemented yet.";
-oken}) {
+# oken}) {
 #     }
 #     else {
 # 	die "No SSO token acquired, you must log in first.";
 #     }
 # }
-
+}
 
 sub login {
     my ($self, $username, $password) = @_;
@@ -102,6 +104,7 @@ sub logout {
 # 	$self->{sso_token} = undef;
 #     }
 # }
+}
 
 sub register_urn {
     my ($self, $urn, $url) = @_;
@@ -118,12 +121,13 @@ sub replace_url {
     my ($self, $urn, $oldURL, $newURL) = @_;
 
 #     if (defined $self->{sso_tdie "Not implemented yet.";
-oken}) {
+# oken}) {
 #     }
 #     else {
 # 	die "No SSO token acquired, you must log in first.";
 #     }
 # }
+}
 
 sub reserve_next_urn {
     my ($self, $seriesCode) = @_;
